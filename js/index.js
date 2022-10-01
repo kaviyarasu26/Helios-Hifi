1e181eafa2a02bb67a222077ba7723ac1f8d1b9c
kaviyarasu26 <101913405+kaviyarasu26@users.noreply.github.com>
2022-10-01 10:37:41 +0530

    js for story mode

--- js/index.js
+++ js/index.js
@@ -1,21 +1,18 @@
-var kavi = document.getElementById("aside");
-        function summada() {
-            if (kavi.style.display == "block") {
-                kavi.style.display = "none";
-            }
-            else {
-                kavi.style.display = "block";
-            }
 
-        }
 
 //start
 //story
-var home= document.getElementById("home");
-var time_line= document.getElementById("time-line");
-var story= document.getElementById("");
+var homepage=document.getElementById("king");
+var time_line=document.getElementById("time-line");
+var story_mode=document.getElementById("story-mode");
+var about=document.getElementById("about");
 
+function story(){
+    homepage.style.display="none";
+    time_line.style.display="none";
+    story_mode.style.display="block";
+    about.style.display="none";
 
+}
 
-function
         
