5163aea27096d3d8daab3e21e31f945d04cf492e
kaviyarasu26 <101913405+kaviyarasu26@users.noreply.github.com>
2022-10-01 11:20:26 +0530

    pre loader

--- js/index.js
+++ js/index.js
@@ -6,6 +6,11 @@ var homepage=document.getElementById("king");
 var time_line=document.getElementById("time-line");
 var story_mode=document.getElementById("story-mode");
 var about=document.getElementById("about");
+var prescreen =document.querySelector(".loader");
+window.addEventListener("load",vanish);
+function vanish(){
+    prescreen.classList.add("disppers");
+}
 
 function story(){
     homepage.style.display="none";
