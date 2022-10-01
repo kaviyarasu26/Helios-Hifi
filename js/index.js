4d739e1e60b65e55a23e13d3105e68119a23a315
kaviyarasu26 <101913405+kaviyarasu26@users.noreply.github.com>
2022-10-01 22:49:47 +0530

    home page responsive

--- js/index.js
+++ js/index.js
@@ -6,6 +6,22 @@ var homepage=document.getElementById("king");
 var time_line=document.getElementById("time-line");
 var story_mode=document.getElementById("story-mode");
 var about=document.getElementById("about");
+var menu=document.getElementsByClassName("menu");
+
+
+
+var aside = document.getElementById("aside");
+function summada() {
+    if (aside.style.display == "block") {
+        aside.style.display = "none";
+    }
+    else {
+        aside.style.display = "block";
+    }
+
+}
+
+
 var prescreen =document.querySelector(".loader");
 window.addEventListener("load",vanish);
 function vanish(){
@@ -29,4 +45,11 @@ function aboutus(){
 
 }
 
+function timelinego(){
+    homepage.style.display="none";
+    time_line.style.display="block";
+    story_mode.style.display="none";
+    about.style.display="none";
+
+}
         
